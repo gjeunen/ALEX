6891ce9498ff1b16c1f5a479d6af2e2b56ed3c14
gjeunen <gjeunen@gmail.com>
2025-09-24 10:41:39 +0100

    using db for taxonomic lineage

--- function/alex_functions.py
+++ function/alex_functions.py
@@ -86,27 +86,60 @@ def blast_to_memory(console, columns, blast_input_):
     uniq_species_list = []
     total_size = os.path.getsize(blast_input_)
     with rich.progress.Progress(*columns) as progress_bar:
-        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Extracting...[/] |", total=total_size)
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Reading BLAST[/] |", total=total_size)
         with open(blast_input_, 'r') as infile:
             for line in infile:
                 progress_bar.update(pbar, advance = len(line))
                 if line == 'not assigned\n':
                     continue
-                seq_id = line.split('\t')[0]
-                species_name = line.split('\t')[3]
+                seq_id = line.split('\t')[0].split(';')[0]
+                tax_id = line.split('\t')[2]
                 pident = float(line.split('\t')[5])
                 qcov = int(line.split('\t')[7])
                 if seq_id not in blast_dict:
                     blast_dict[seq_id]['pident'] = pident
                     blast_dict[seq_id]['qcov'] = qcov
-                    species_dict[seq_id].append(species_name)
-                    if species_name not in uniq_species_list:
-                        uniq_species_list.append(species_name)
-                elif pident >= blast_dict[seq_id]['pident'] and qcov >= blast_dict[seq_id]['qcov'] and species_name not in species_dict[seq_id] and species_name != 'N/A':
-                    species_dict[seq_id].append(species_name)
-                    if species_name not in uniq_species_list:
-                        uniq_species_list.append(species_name)
+                    species_dict[seq_id].append(tax_id)
+                    if tax_id not in uniq_species_list:
+                        uniq_species_list.append(tax_id)
+                elif pident == blast_dict[seq_id]['pident'] and qcov == blast_dict[seq_id]['qcov'] and tax_id not in species_dict[seq_id] and tax_id != 'N/A':
+                    species_dict[seq_id].append(tax_id)
+                    if tax_id not in uniq_species_list:
+                        uniq_species_list.append(tax_id)
+                elif pident > blast_dict[seq_id]['pident'] and qcov == blast_dict[seq_id]['qcov']:
+                    blast_dict[seq_id]['pident'] = pident
+                    blast_dict[seq_id]['qcov'] = qcov
+                    species_dict[seq_id] = [tax_id]
+                    if tax_id not in uniq_species_list:
+                        uniq_species_list.append(tax_id)
+                elif pident == blast_dict[seq_id]['pident'] and qcov > blast_dict[seq_id]['qcov']:
+                    blast_dict[seq_id]['pident'] = pident
+                    blast_dict[seq_id]['qcov'] = qcov
+                    species_dict[seq_id] = [tax_id]
+                    if tax_id not in uniq_species_list:
+                        uniq_species_list.append(tax_id)
+                elif pident > blast_dict[seq_id]['pident'] and qcov > blast_dict[seq_id]['qcov']:
+                    blast_dict[seq_id]['pident'] = pident
+                    blast_dict[seq_id]['qcov'] = qcov
+                    species_dict[seq_id] = [tax_id]
+                    if tax_id not in uniq_species_list:
+                        uniq_species_list.append(tax_id)
     return blast_dict, species_dict, uniq_species_list
+
+def fasta_to_memory(console, columns, fasta_):
+    '''
+    reads a fasta file of ASVs into memory and returns a list of sequence IDs
+    '''
+    zotu_list = []
+    total_size = os.path.getsize(fasta_)
+    with rich.progress.Progress(*columns) as progress_bar:
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|        Reading list[/] |", total=total_size)
+        with open(fasta_, 'r') as infile:
+            for line in infile:
+                progress_bar.update(pbar, advance = len(line))
+                if line.startswith('>'):
+                    zotu_list.append(line.split(';')[0].lstrip('>'))
+    return zotu_list
 
 def table_to_memory(console, columns, table_input_):
     '''
@@ -115,7 +148,7 @@ def table_to_memory(console, columns, table_input_):
     zotu_list = []
     total_size = os.path.getsize(table_input_)
     with rich.progress.Progress(*columns) as progress_bar:
-        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Extracting...[/] |", total=total_size)
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Reading table[/] |", total=total_size)
         count = 0
         with open(table_input_, 'r') as infile:
             for line in infile:
@@ -132,7 +165,7 @@ def fill_out_blast_dict(console, columns, zotu_list, blast_dict, species_dict):
     '''
     total_size = len(zotu_list)
     with rich.progress.Progress(*columns) as progress_bar:
-        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Extracting...[/] |", total=total_size)
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|   Fill missing data[/] |", total=total_size)
         for item in zotu_list:
             progress_bar.update(pbar, advance = 1)
             if item not in blast_dict:
@@ -140,6 +173,83 @@ def fill_out_blast_dict(console, columns, zotu_list, blast_dict, species_dict):
                 blast_dict[item]['qcov'] = 'NA'
                 species_dict[item].append('NA')
     return blast_dict
+
+def crabs_to_memory(console, columns, crabs_, uniq_species_list):
+    '''
+    reads crabs_ to memory and extracts the necessary phylogenetic information
+    '''
+    species_lineages_dict = collections.defaultdict(dict)
+    total_size = os.path.getsize(crabs_)
+    with rich.progress.Progress(*columns) as progress_bar:
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|       Reading crabs[/] |", total=total_size)
+        with open(crabs_, 'r') as infile:
+            for line in infile:
+                progress_bar.update(pbar, advance = len(line))
+                tax_id = line.split('\t')[2]
+                if tax_id in uniq_species_list:
+                    if tax_id not in species_lineages_dict:
+                        species_lineages_dict[tax_id]['kingdom'] = line.split('\t')[3]
+                        species_lineages_dict[tax_id]['phylum'] = line.split('\t')[4]
+                        species_lineages_dict[tax_id]['class'] = line.split('\t')[5]
+                        species_lineages_dict[tax_id]['order'] = line.split('\t')[6]
+                        species_lineages_dict[tax_id]['family'] = line.split('\t')[7]
+                        species_lineages_dict[tax_id]['genus'] = line.split('\t')[8]
+                        species_lineages_dict[tax_id]['species'] = line.split('\t')[9]
+                        species_lineages_dict[tax_id]['common'] = line.split('\t')[1]
+    return species_lineages_dict
+
+def create_mrca(console, columns, species_dict, species_lineages_dict):
+    '''
+    looks up the taxonomic lineages for each sequence ID and returns the MRCA
+    '''
+    mrca_dict = {}
+    contributing_species = collections.defaultdict(list)
+    with rich.progress.Progress(*columns) as progress_bar:
+        pbar = progress_bar.add_task(console = console, description = "[cyan]|    Calculating MRCA[/] |", total=len(species_dict))
+        for item in species_dict:
+            progress_bar.update(pbar, advance = 1)
+            if len(species_dict[item]) == 1:
+                lineage = species_lineages_dict[species_dict[item][0]]
+                mrca_dict[item] = lineage
+                try:
+                    if lineage['species'] != 'NA':
+                        contributing_species[item].append(lineage['species'])
+                    else:
+                        contributing_species[item].append(lineage['common'])
+                except KeyError:
+                    contributing_species[item].append('NA')
+            else:
+                count = 0
+                mrca_lineage = []
+                for tax_id in species_dict[item]:
+                    lineage = species_lineages_dict[tax_id]
+                    if lineage.get('species', 'NA') != 'NA':
+                        contributing_species[item].append(lineage['species'])
+                    else:
+                        contributing_species[item].append(lineage.get('common', 'NA'))
+                    count += 1
+                    if count == 1:
+                        mrca_lineage = lineage.copy()
+                    else:
+                        for key in mrca_lineage:
+                            if key not in lineage or mrca_lineage[key] != lineage[key]:
+                                mrca_lineage[key] = 'NA'
+                mrca_dict[item] = mrca_lineage
+    return mrca_dict, contributing_species
+
+def write_output2(blast_dict, mrca_dict, contributing_species, output_):
+    '''
+    write results to output file
+    '''
+    separator = '\t'
+    ranks_needed = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+    with open(output_, 'w') as outfile:
+        outfile.write(f'#OTU ID\t{separator.join(ranks_needed)}\tpident\tqcov\tmatching species IDs\n')
+        for item in blast_dict:
+            try:
+                outfile.write(f'{item}\t{mrca_dict[item][ranks_needed[0]]}\t{mrca_dict[item][ranks_needed[1]]}\t{mrca_dict[item][ranks_needed[2]]}\t{mrca_dict[item][ranks_needed[3]]}\t{mrca_dict[item][ranks_needed[4]]}\t{mrca_dict[item][ranks_needed[5]]}\t{mrca_dict[item][ranks_needed[6]]}\t{blast_dict[item]["pident"]}\t{blast_dict[item]["qcov"]}\t{", ".join(contributing_species[item])}\n')
+            except KeyError:
+                outfile.write(f'{item}\tNA\tNA\tNA\tNA\tNA\tNA\tNA\tNA\tNA\tNA\n')
 
 def names_to_memory(console, columns, names_input_):
     '''
